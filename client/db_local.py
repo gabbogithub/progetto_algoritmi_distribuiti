a3257c7872f956c4f80bf33132c8fba1eeebc4eb
Gabriel Panini <gabrielpanini2@gmail.com>
2025-07-21 00:39:20 +0200

    Add first implementation of the local operations

--- client/db_local.py
+++ client/db_local.py
@@ -4,7 +4,7 @@ from pykeepass import PyKeePass, create_database
 class DBLocal():
 
     def __init__(self, path: str, passwd: str) -> None:
-        self._pk_db = PyKeePass(path, passwd)
+        self._kp_db = PyKeePass(path, passwd)
 
     @classmethod
     def create_db(cls, path: str, passwd: str) -> Self:
@@ -12,24 +12,48 @@ class DBLocal():
         return cls(path, passwd)
 
     def reset_db(self, path: str, passwd: str) -> None:
-        self._pk_db = PyKeePass(path, passwd)
+        self._kp_db = create_database(path, passwd)
 
-    def add_entry(self):
-        pass
+    def add_entry(self, destination_group, title: str, username: str, passwd: str):
+        group = self._kp_db.find_groups(path=destination_group, first=True)
+        if group is None:
+            raise KeyError("The group for the entry doesn't exist!")
+        
+        if self._kp_db.find_entries(group=group, title=title) is not None:
+            raise KeyError("The entry under the specified group, with the specified title already exists!")
+        
+        self._kp_db.add_entry(group, title, username, passwd)
 
-    def add_group(self, parent_group: str, group_name: str) -> None:
-        self._pk_db.add_group(parent_group, group_name)
-        self._pk_db.save()
+    def add_group(self, parent_group: list[str], group_name: str) -> None:
+        parent = self._kp_db.find_groups(path=parent_group, first=True)
 
-    def delete_entry(self):
-        pass
+        if parent is None:
+            raise ValueError(f"The parent group does not exist!")
 
-    def delete_group(self):
-        pass
+        if self._kp_db.find_groups(group=parent, name=group_name, first=True, recursive=False) is not None:
+            raise ValueError("The group is already present in the parent group!")
+
+        self._kp_db.add_group(parent, group_name) 
+
+    def delete_entry(self, entry_path: list[str]) -> None:
+        entry = self._kp_db.find_entries(path=entry_path, first=True)
+        if entry is None:
+            raise KeyError("The entry doesn't exist!")
+
+        self._kp_db.delete_entry(entry)
+
+    def delete_group(self, path: list[str]) -> None:
+        group = self._kp_db.find_groups(path=path, first=True) 
+        if group is None:
+            raise KeyError("The group doesn't exist!")
+
+        self._kp_db.delete_group(group)
     
     def change_name(self, new_name: str) -> None:
-        self._pk_db.database_name = new_name
-        self._pk_db.save()
+        self._kp_db.database_name = new_name
 
     def get_entries(self):
-        return self._pk_db.entries
+        return self._kp_db.entries
+    
+    def save_changes(self) -> None:
+        self._kp_db.save()
--- main.py
+++ main.py
@@ -3,17 +3,15 @@ from pykeepass import PyKeePass, create_database
 from client.db_local import DBLocal
 
 def main():
-    """
     db_name = input("Inserisci il nome del database: ")
     try:
         db_pass = getpass.getpass(prompt="Inserisci la password del database: ")
+        db = DBLocal(db_name, db_pass)
+        for i in range(1000):
+            db.add_group2([""], f"{i}")
+        db.save_changes()
     except Exception as error:
         print('ERROR', error)
-    else:
-    """
-    db = DBLocal("prova", "prova")
-    print(db._pk_db.find_groups(name="Root"))
-        #db.add_group("root", "gruppone")
 
 if __name__ == "__main__":
     main()
